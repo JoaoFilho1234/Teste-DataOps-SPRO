351f606cc61971e578ba04169dc53e4265ae56b3
JoaoFilho1234 <joaofilho.jbf28@gmail.com>
2023-08-17 04:38:58 -0300

    Salvando Pandas DataFrame no MongoDB

--- dataFrame.py
+++ dataFrame.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pymongo import MongoClient
 
 #Criando dataframe Carro
 
@@ -16,7 +17,19 @@ df1 = pd.DataFrame(data1)
 data2 = {
     'Montadora': ['Chevrolet', 'Volkswagen', 'Renault', 'Ford', 'Honda'],
     'País': ['EUA', 'Alemanhã', 'França', 'EUA', 'Japão']
-
 }
 
 df2 = pd.DataFrame(data2)
+
+client = MongoClient('mongodb://localhost', 27017)
+db = client['local']
+collection1 = db['Carros']
+collection2 = db['Montadoras']
+
+data_to_insert1 = df1.to_dict(orient='records')
+data_to_insert2 = df2.to_dict(orient='records')
+
+collection1.insert_many(data_to_insert1)
+collection2.insert_many(data_to_insert2)
+
+client.close()
